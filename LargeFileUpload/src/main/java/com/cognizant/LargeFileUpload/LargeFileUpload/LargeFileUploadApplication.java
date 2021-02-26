5934a525853db16ded6148e5989efddb007a7ea6
761104 <761104@LTIN171425.cts.com>
2021-02-26 19:57:13 +0530

    LC

---
+++ LargeFileUpload/src/main/java/com/cognizant/LargeFileUpload/LargeFileUpload/LargeFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.LargeFileUpload.LargeFileUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeFileUploadApplication.class, args);
+	}
+
+}
---
+++ LargeFileUpload/src/test/java/com/cognizant/LargeFileUpload/LargeFileUpload/LargeFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.LargeFileUpload.LargeFileUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
